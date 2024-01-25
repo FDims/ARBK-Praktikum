64aa010052f8bd29437679d2dae65b2936030464
fp5774s <fachrialdimaspp15@gmail.com>
2024-01-25 20:52:51 +0100

    update

--- Praktikum5/Fachrial D P Perdana_3503937_ARBKVS_Praktikum5/Fachrial D P Perdana_3503937_ARBKVS_Praktikum5/Semaphore.h
+++ Praktikum5/Fachrial D P Perdana_3503937_ARBKVS_Praktikum5/Fachrial D P Perdana_3503937_ARBKVS_Praktikum5/Semaphore.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #ifndef SEMAPHORE_H
 #define SEMAPHORE_H
 
@@ -32,3 +33,39 @@ public:
 };
 
 #endif // !SEMAPHORE_H
+=======
+#ifndef SEMAPHORE_H
+#define SEMAPHORE_H
+
+#include <mutex>
+#include <condition_variable>
+
+
+class Semaphore {
+private:
+	std::mutex Mutex;
+	std::condition_variable cVar;
+	int var;
+
+public:
+	Semaphore(int _var = 1) {
+		var = _var;
+	}
+
+	void post() {
+		var++;
+
+		cVar.notify_one();
+	}
+
+	void wait() {
+		std::unique_lock<std::mutex> lock(Mutex);
+		if (var <= 0)
+			cVar.wait(lock);
+		if (var > 0)
+			var-- ;
+	}
+};
+
+#endif // !SEMAPHORE_H
+>>>>>>> Stashed changes
--- Praktikum5/Fachrial D P Perdana_3503937_ARBKVS_Praktikum5/Fachrial D P Perdana_3503937_ARBKVS_Praktikum5/main.cpp
+++ Praktikum5/Fachrial D P Perdana_3503937_ARBKVS_Praktikum5/Fachrial D P Perdana_3503937_ARBKVS_Praktikum5/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -85,4 +86,93 @@ int main() {
 	MutexThreading();
 
 	return 0;
+=======
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include "Semaphore.h"
+
+using namespace std;
+
+mutex Mutex;
+Semaphore semaphore;
+
+void threadT1() {
+	Mutex.lock();
+	for (char i = 'a'; i <= 'z'; i++) {
+		cout << i << ", ";
+	}
+	cout << endl;
+	Mutex.unlock();
+}
+
+void threadT2() {
+	Mutex.lock();
+	for (int i = 0; i <= 32; i++) {
+		cout << i << ", ";
+	}
+	cout << endl;
+	Mutex.unlock();
+}
+
+void threadT3() {
+	Mutex.lock();
+	for (char i = 'A'; i <= 'Z'; i++) {
+		cout << i << ", ";
+	}
+	cout << endl;
+	Mutex.unlock();
+}
+
+void MutexThreading() {
+	thread T1(threadT1);
+	thread T2(threadT2);
+	thread T3(threadT3);
+	//T1.join();
+	//T2.join();
+	//T3.join();
+}
+
+
+
+void threadT1S() {
+	semaphore.wait();
+	for (char i = 'a'; i <= 'z'; i++) {
+		cout << i << ", ";
+	}
+	cout << endl;
+	semaphore.post();
+}
+void threadT2S() {
+	semaphore.wait();
+	for (int i = 0; i <= 32; i++) {
+		cout << i << ", ";
+	}
+	cout << endl;
+	semaphore.post();
+}
+void threadT3S() {
+	semaphore.wait();
+	for (char i = 'A'; i <= 'Z'; i++) {
+		cout << i << ", ";
+	}
+	cout << endl;
+	semaphore.post();
+}
+
+void SemaphoreThreading() {
+	thread T1(threadT1S);
+	thread T2(threadT2S);
+	thread T3(threadT3S);
+	T1.join();
+	T2.join();
+	T3.join();
+}
+
+int main() {
+	//SemaphoreThreading();
+	MutexThreading();
+
+	return 0;
+>>>>>>> Stashed changes
 }
